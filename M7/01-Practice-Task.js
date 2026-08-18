562f05da92227b56e13f8fdab3f146b44f8d75f3
Rahat Karim <rahatkarim5452@gmail.com>
2026-08-18 20:19:59 +0600

    first commit

---
+++ M7/01-Practice-Task.js
@@ -0,0 +1,4 @@
+//TODO: Create a file named app.js and print three introductory messages.
+console.log("Hello JavaScript!");
+console.log("My Name is Alex");
+console.log("I am Excited to bracome a Web Developer.");
---
+++ M7/03-Practice-Task.js
@@ -0,0 +1,11 @@
+//TODO: Store student details in variables of the appropriate type and print them.
+let name = "Rahat Karim";
+let age = 26;
+let country = "Bangladesh";
+let isStudent = true;
+
+//Printing Details
+console.log(name);
+console.log(age);
+console.log(country);
+console.log(isStudent);

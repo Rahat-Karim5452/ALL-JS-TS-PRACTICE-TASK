fa923748a816eba3e8379448cc21bd67d2a9152b
Rahat Karim <rahatkarim5452@gmail.com>
2026-08-20 22:30:34 +0600

    CS problem 4

---
+++ src/CS-TS-1/4-calculateOrderTotal.ts
@@ -0,0 +1,20 @@
+interface Items {
+  name: string;
+  price: number;
+}
+const calculateOrderTotal = (items: Items[]): number => {
+  const total: number = items.reduce(
+    (sum: number, item: Items) => sum + item.price,
+    0,
+  );
+  return total;
+};
+console.log(
+  calculateOrderTotal([
+    { name: "Burger", price: 250 },
+    { name: "Fries", price: 90 },
+    { name: "Soda", price: 60 },
+  ]),
+);
+console.log(calculateOrderTotal([{ name: "Pizza", price: 500 }]));
+console.log(calculateOrderTotal([]));

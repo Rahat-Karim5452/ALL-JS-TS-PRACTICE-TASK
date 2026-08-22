8c4ef9bb7f1cc0ed8e7d3bce001d87b93debabfd
Rahat Karim <rahatkarim5452@gmail.com>
2026-08-22 23:54:53 +0600

    A4 1 and Final Modified

--- src/M25-A4/Final.ts
+++ src/M25-A4/Final.ts
@@ -3,11 +3,11 @@ const getBatteryStatus = (percentage: number): string => {
   if (percentage < 0 || percentage > 100) {
     return "Invalid Number";
   }
-  if (percentage >= 0 && percentage <= 20) {
+  if (percentage <= 20) {
     return "Low";
-  } else if (percentage >= 21 && percentage <= 50) {
+  } else if (percentage <= 50) {
     return "Medium";
-  } else if (percentage >= 51 && percentage <= 90) {
+  } else if (percentage <= 90) {
     return "High";
   }
   return "Full";

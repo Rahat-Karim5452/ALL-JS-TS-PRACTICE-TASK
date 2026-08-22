f60be073a62d31138a5db4aa77faea9f1b99a1e9
Rahat Karim <rahatkarim5452@gmail.com>
2026-08-23 00:55:27 +0600

    M18 task - 5

---
+++ M18/task-5.js
@@ -0,0 +1,16 @@
+//Nested Destructuring
+const getEmployeeDept = (employee) => {
+  const {
+    job: { title, department },
+  } = employee;
+  return {
+    title,
+    department,
+  };
+};
+console.log(
+  getEmployeeDept({
+    name: "Nadia",
+    job: { title: "PM", department: "Product" },
+  }),
+);

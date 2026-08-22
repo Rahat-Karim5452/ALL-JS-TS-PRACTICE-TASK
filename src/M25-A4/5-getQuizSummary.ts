d4194af5194d882366f7869180c4444a88bae306
Rahat Karim <rahatkarim5452@gmail.com>
2026-08-22 22:57:21 +0600

    M25 A -5 modified

--- src/M25-A4/5-getQuizSummary.ts
+++ src/M25-A4/5-getQuizSummary.ts
@@ -1,3 +1,7 @@
+interface Result {
+  total: number;
+  average: number;
+}
 const getQuizSummary = (scores: number[]): Result => {
   let total = scores.reduce((sum, num) => sum + num, 0);
   let average = total / scores.length;
@@ -6,10 +10,6 @@ const getQuizSummary = (scores: number[]): Result => {
     average: isNaN(average) ? 0 : average,
   };
 };
-interface Result {
-  total: number;
-  average: number;
-}
 console.log(getQuizSummary([8, 9, 7, 10]));
 console.log(getQuizSummary([5, 5]));
 console.log(getQuizSummary([]));

96e94bf33e35f1eda46f3ca994fec85ce3cd65ee
Rahat Karim <rahatkarim5452@gmail.com>
2026-08-24 23:29:24 +0600

    Type A  frist Half Type A-7,8  JS-CS-2

--- JS-CS-2/Type-A/15.js
+++ JS-CS-2/Type-A/15.js
@@ -1,1 +1,5 @@
 // 7. Write a function that takes a string as a parameter and returns the number of characters (its length).
+function checkStringLength(str) {
+  return str.length;
+}
+console.log(checkStringLength("Hello World"));
--- JS-CS-2/Type-A/16.js
+++ JS-CS-2/Type-A/16.js
@@ -1,1 +1,12 @@
 // 8. Write a function that takes an array of numbers as a parameter and returns the average of all the elements.
+function sumOfArray(nums) {
+  let sum = 0;
+  let avg = 0;
+  for (let num of nums) {
+    // console.log(num);
+    sum += num;
+  }
+  avg = sum / nums.length;
+  return avg;
+}
+console.log(sumOfArray([2, 2, 3, 4, 5]));

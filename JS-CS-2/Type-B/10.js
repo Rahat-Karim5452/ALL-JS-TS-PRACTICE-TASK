9c5e54d57276fcd238ef1a84178dbf7ac8db8c70
Rahat Karim <rahatkarim5452@gmail.com>
2026-08-24 23:36:23 +0600

    Type B  Second Half Type B-1,2  JS-CS-2

---
+++ JS-CS-2/Type-B/10.js
@@ -0,0 +1,5 @@
+// 2. Write a function that takes the length and width of a rectangle as parameters and returns its area.
+function calculateArea(len, wdth) {
+  return len * wdth;
+}
+console.log(calculateArea(10, 20));

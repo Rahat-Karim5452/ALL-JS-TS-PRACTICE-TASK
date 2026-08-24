a3ce372fb236ab7789d16bae858ddd38af87632c
Rahat Karim <rahatkarim5452@gmail.com>
2026-08-24 23:17:13 +0600

    Type A  frist Half Type A-3,4  JS-CS-2

--- JS-CS-2/Type-A/11.js
+++ JS-CS-2/Type-A/11.js
@@ -1,2 +1,7 @@
 // 3. Write a function with a default parameter for tax rate (5%) that calculates and returns the total price of an item including
 // tax.
+function calculateTotalPrice(price, taxRate = 5) {
+  let tax = (price * taxRate) / 100;
+  return price + tax;
+}
+console.log(calculateTotalPrice(100));
--- JS-CS-2/Type-A/12.js
+++ JS-CS-2/Type-A/12.js
@@ -1,1 +1,8 @@
 // 4. Write a function that takes a number as a parameter and returns whether it is 'Even' or 'Odd'.
+function checkOddEven(numbers) {
+  if (numbers % 2 === 0) return "Even";
+  return "Odd";
+}
+console.log(checkOddEven(2));
+console.log(checkOddEven(1));
+console.log(checkOddEven(5));

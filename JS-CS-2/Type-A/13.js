e5e7595ae920ca03ac8a46ff4190a4eb2d5253b5
Rahat Karim <rahatkarim5452@gmail.com>
2026-08-24 23:24:17 +0600

    Type A  frist Half Type A-5,6  JS-CS-2

--- JS-CS-2/Type-A/13.js
+++ JS-CS-2/Type-A/13.js
@@ -1,1 +1,10 @@
 // 5. Write a function that takes an array of numbers as a parameter and returns the sum of all the elements.
+function sumOfArray(nums) {
+  let sum = 0;
+  for (let num of nums) {
+    // console.log(num);
+    sum += num;
+  }
+  return sum;
+}
+console.log("Sum of Array Elem: ", sumOfArray([2, 2, 3, 4, 5]));
--- JS-CS-2/Type-A/14.js
+++ JS-CS-2/Type-A/14.js
@@ -1,1 +1,7 @@
 // 6. Write a function that takes three numbers as parameters and returns the largest of the three.
+function checkNumber(num1, num2, num3) {
+  if (num1 > num2 && num1 > num3) return "largest: " + num1;
+  else if (num2 > num1 && num2 > num3) return "largest: " + num2;
+  return "largest: " + num3;
+}
+console.log(checkNumber(1, 10, 2));

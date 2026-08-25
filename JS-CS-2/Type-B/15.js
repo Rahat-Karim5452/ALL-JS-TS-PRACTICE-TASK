1343282a03e325270648cf1d1237c5e4a222df53
Rahat Karim <rahatkarim5452@gmail.com>
2026-08-25 12:58:20 +0600

    Type B  Second Half Type B-7,8  JS-CS-2

--- JS-CS-2/Type-B/15.js
+++ JS-CS-2/Type-B/15.js
@@ -1,2 +1,12 @@
 // 7. Write a function that takes an array of numbers as a parameter and returns a new array containing only the numbers
 // greater than 10.
+function checkGrater(arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 10) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+console.log(checkGrater([10, 20, 31, 49, 50, 57, 20, 509]));
--- JS-CS-2/Type-B/16.js
+++ JS-CS-2/Type-B/16.js
@@ -1,1 +1,6 @@
 // 8. Write a function that takes two numbers as parameters and returns the larger of the two without using Math.max().
+function checkLargest(num1, num2) {
+  if (num1 > num2) return num1;
+  return num2;
+}
+console.log(checkLargest(20, 21));

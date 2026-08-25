c44ea8337e947c9ab9bd6e70b48873cd4a2b9af3
Rahat Karim <rahatkarim5452@gmail.com>
2026-08-25 13:33:51 +0600

    M8 Practice Task 7,8,9,10  Done

---
+++ M8/10-Practice-Task.js
@@ -0,0 +1,9 @@
+/* Build a simple login system — print "Login successful" only if both username and
+password are correct. */
+let username = "rahat";
+let password = 12345;
+if (username === "rahat" && password === 12345) {
+  console.log("Login Successful");
+} else {
+  console.log("Try again");
+}

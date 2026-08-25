a62974460139eddb8b428c3b5d1b55ec155f1793
Rahat Karim <rahatkarim5452@gmail.com>
2026-08-25 23:25:44 +0600

    M11 practice task 3

---
+++ M11/2.1_Case_Conversion.js
@@ -0,0 +1,14 @@
+/* Task 2.1 — Case Conversion
+let name = "JavaScript";
+● Print the uppercase version.
+● Print the lowercase version.
+● Print the string with whitespace trimmed: " hi there " → "hi there"
+*/
+let name = "JavaScript";
+let uppercaseVersion = name.toUpperCase();
+console.log(uppercaseVersion);
+let lowercaseVersion = name.toLowerCase();
+console.log(lowercaseVersion);
+let str = " hi there ";
+
+console.log(str.trim());

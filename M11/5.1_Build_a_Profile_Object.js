11a68e4d380ab8b7a029e85d53f38ced51001bd0
Rahat Karim <rahatkarim5452@gmail.com>
2026-08-26 00:39:08 +0600

    M11 practice task 5.1

---
+++ M11/5.1_Build_a_Profile_Object.js
@@ -0,0 +1,13 @@
+/* Task 5.1 — Build a Profile Object
+Create an object student with these properties: name, age, grade, and isEnrolled.
+● Log the whole object.
+● Log just the name property using dot notation.
+*/
+let profile = {
+  name: "Rahat",
+  age: 23,
+  grade: "A",
+  isEnrolled: true,
+};
+console.log(profile);
+console.log(profile.name);

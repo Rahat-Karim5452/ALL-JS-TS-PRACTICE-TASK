cb5fc9eedd9334365981a0275a35104951cdfa57
Rahat Karim <rahatkarim5452@gmail.com>
2026-08-25 20:55:27 +0600

    M9 Practice task 6  to 10  Solved

---
+++ M9/09-Practice-Task.js
@@ -0,0 +1,9 @@
+/* Task 9 — Sum of Even Numbers
+Write a for loop that calculates the sum of all even numbers from 2 to 50. */
+let sum = 0;
+for (let i = 1; i <= 50; i++) {
+  if (i % 2 === 0) {
+    sum += i;
+  }
+}
+console.log(sum);

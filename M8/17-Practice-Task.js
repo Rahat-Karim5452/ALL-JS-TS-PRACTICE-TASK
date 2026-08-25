b27e6d23c232cef34d8e86fe92dea3931c6a9883
Rahat Karim <rahatkarim5452@gmail.com>
2026-08-25 19:54:59 +0600

    M8 Practice Task 17,18  Done

---
+++ M8/17-Practice-Task.js
@@ -0,0 +1,3 @@
+//TODO:Using age, determine "Adult" or "Minor" using the ternary operator.
+let age = 30;
+console.log(age >= 18 ? "Adult" : "Minor");
---
+++ M8/18-Practice-Task.js
@@ -0,0 +1,4 @@
+//TODO: If the price is greater than 1000, print "Expensive", otherwise "Affordable" — write this
+//TODO: using a ternary operator.
+let price = 1100;
+console.log(price > 1000 ? "Expensive" : "Affordable");

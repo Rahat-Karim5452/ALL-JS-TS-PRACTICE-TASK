f1ef790f84349d5a0428ed7a41b8eb3028a9e1d4
Rahat Karim <rahatkarim5452@gmail.com>
2026-08-25 23:01:24 +0600

    M10 advanced practice 3 done

--- M10/Advanced/03-Practice-Task.js
+++ M10/Advanced/03-Practice-Task.js
@@ -1,2 +1,15 @@
 // 3. Given an array of numbers, find the largest and smallest value without using
 // Math.max or Math.min directly on the whole array (use a loop).
+let numbers = [11, 10, 2, 3, 4, 5];
+let largest = numbers[0];
+let smallest = numbers[0];
+for (let i = 0; i < numbers.length; i++) {
+  if (largest < numbers[i]) {
+    largest = numbers[i];
+  }
+  if (smallest > numbers[i]) {
+    smallest = numbers[i];
+  }
+}
+console.log(largest);
+console.log(smallest);

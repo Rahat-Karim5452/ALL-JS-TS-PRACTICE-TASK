dcce8a850a53c1236bc645038c9d62f94e995218
Rahat Karim <rahatkarim5452@gmail.com>
2026-08-25 13:23:45 +0600

    M8 Practice Task 5,6  Done

---
+++ M8/05-Practice-Task.js
@@ -0,0 +1,7 @@
+//TODO: Take a number variable and check whether it is positive or negative (using if-else).
+let num = 10;
+if (num < 0) {
+  console.log("Negative");
+} else {
+  console.log("Positive");
+}
---
+++ M8/06-Practice-Task.js
@@ -0,0 +1,5 @@
+//TODO:Take a year variable and check whether it is a leap year (hint: year % 4 === 0).
+let year = 2000;
+if (year % 4 === 0) {
+  console.log("Leap Year");
+}

b65be53043e0d101f2329183dc37a8af76a878dd
Rahat Karim <rahatkarim5452@gmail.com>
2026-08-25 22:57:14 +0600

    M10 advanced practice 2 done

--- M10/Advanced/02-Practice-Task.js
+++ M10/Advanced/02-Practice-Task.js
@@ -1,2 +1,14 @@
 // 2. Given an array of mixed values (numbers and strings), separate them into two arrays
 // and log both.
+let numbers = [1, 1, 2, 3, 4, 5, "string", "string1"];
+let numberArray = [];
+let stringArray = [];
+for (let i = 0; i < numbers.length; i++) {
+  if (typeof numbers[i] === "number") {
+    numberArray.push(numbers[i]);
+  } else {
+    stringArray.push(numbers[i]);
+  }
+}
+console.log(numberArray);
+console.log(stringArray);

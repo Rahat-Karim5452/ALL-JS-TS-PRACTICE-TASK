a872f32714035f4e0dba823414f934bc64dd82b2
Rahat Karim <rahatkarim5452@gmail.com>
2026-08-25 13:16:53 +0600

    M8 Practice Task 1,2 Done

---
+++ M8/01-Practice-Task.js
@@ -0,0 +1,12 @@
+// Take two variables price1 and price2, and compare them using all comparison operators
+//(==, ===, !=, !==, >, <, >=, <=), printing each result with console.log.
+let price1 = 20;
+let price2 = 30;
+console.log(price1 == price2);
+console.log(price1 === price2);
+console.log(price1 != price2);
+console.log(price1 !== price2);
+console.log(price1 > price2);
+console.log(price1 >= price2);
+console.log(price1 < price2);
+console.log(price1 <= price2);

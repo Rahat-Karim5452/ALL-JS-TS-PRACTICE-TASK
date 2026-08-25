ab26158c456992d301b8e85ce99caf23772d7766
Rahat Karim <rahatkarim5452@gmail.com>
2026-08-25 19:58:03 +0600

    M8 Practice Task 19,20  Done

---
+++ M8/19-Practice-Task.js
@@ -0,0 +1,5 @@
+//TODO: Using hasPermission, use the ! operator to print "Access Denied" if permission is not granted.
+let hasPermission = true;
+if (!hasPermission) {
+  console.log("Access Deinied");
+}
---
+++ M8/20-Practice-Task.js
@@ -0,0 +1,5 @@
+// todo:Using isOnline, use the NOT operator to print "User is offline".
+let isOnline = true;
+if (!isOnline) {
+  console.log("User is offline");
+}

0c36f03fe4981009e602274ab4ad294ec47d91df
Rahat Karim <rahatkarim5452@gmail.com>
2026-08-27 03:05:58 +0600

    M14 Pratice task 1

---
+++ M14/01-Practice-Task.js
@@ -0,0 +1,14 @@
+/* Below are three small broken snippets. For each, identify the error type (Syntax / Runtime / Logic),
+then fix it.
+
+function sayHi(name) {
+console.log("Hi, " + name / find the bug
+}
+sayHi("Mira");
+
+i/p: sayHi("Mira") => o/p: Hi, Mira
+*/
+function sayHi(name) {
+  console.log("Hi, " + name);
+}
+sayHi("Mira");

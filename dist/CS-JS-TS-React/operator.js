1aeda74a6f30659cb2fac8c61ab1171fd7a3d59f
Rahat Karim <rahatkarim5452@gmail.com>
2026-08-28 18:54:36 +0600

    special session js,cs,react 4.operator

---
+++ dist/CS-JS-TS-React/operator.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// const username = "babul";
+const username = null;
+// const username = undefined;
+const result = username && "useraname is " + username;
+console.log(result);
+//# sourceMappingURL=operator.js.map
---
+++ src/CS-JS-TS-React/operator.ts
@@ -0,0 +1,6 @@
+// const username = "babul";
+const username = null;
+// const username = undefined;
+
+const result = username && "useraname is " + username;
+console.log(result);

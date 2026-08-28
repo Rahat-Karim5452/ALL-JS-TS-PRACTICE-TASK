e5a46bd7b1179d7983d7fc8c6f6fb43abdf2d067
Rahat Karim <rahatkarim5452@gmail.com>
2026-08-28 20:12:29 +0600

    Notes change

--- Notes/map.js
+++ Notes/map.js
@@ -1,10 +1,10 @@
-/* 
-1.map callback function hold Kore.
-2.map er moddhe 3 ta jinis Pai: elements ,index ,array.
-3.Map Method Modify Korar Jnno use Hoy.
-4.tarMane Jotota Element totota 1 Bose new Array diye Dibe
-5.Original Array change kore na 
-*/
+/*
+ * 1.map callback function hold Kore.
+ * 2.map er moddhe 3 ta jinis Pai: elements ,index ,array.
+ * 3.Map Method Modify Korar Jnno use Hoy.
+ * 4.tarMane Jotota Element totota 1 Bose new Array diye Dibe
+ * 5.Original Array change kore na
+ */
 const applyDiscount = (prices) => {
   let pricesAfterDiscount = prices.map((elem, ind, arr) => {
     console.log(elem, ind, arr);
